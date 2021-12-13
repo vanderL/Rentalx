472cc1d32b5709a8a8b990da6c168d256a96ca73
Vander Lima <vnd.vander@gmail.com>
2021-12-12 22:18:34 -0300

    Handle Import File Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.get('/', (request, response) => {
   listCategoryController.handle(request, response);
 });
 
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
 
 export { categoriesRoutes };
