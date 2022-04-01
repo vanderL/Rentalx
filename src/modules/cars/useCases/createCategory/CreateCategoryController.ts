890cb6958fa7ced3b44f7eeec9087d5c9217b425
Vander Lima <vnd.vander@gmail.com>
2022-04-01 16:40:57 -0300

    Apply Inject Dependency with tsyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-  constructor(private createCategoryUserCase: CreateCategoryUseCase) { }
-
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    await this.createCategoryUserCase.execute({ name, description });
+    const createCategoryUserCase = container.resolve(CreateCategoryUseCase);
+
+    await createCategoryUserCase.execute({ name, description });
 
     return response.status(201).send();
   }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import createCategoryController from '../modules/cars/useCases/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
 
@@ -11,11 +11,13 @@ const upload = multer({
   dest: './tmp',
 });
 
+const createCategoryController = new CreateCategoryController();
+
+categoriesRoutes.post('/', createCategoryController.handle);
+
 categoriesRoutes.get('/', (request, response) => {
   listCategoryController.handle(request, response);
 });
-
-categoriesRoutes.post('/', (request, response) => createCategoryController().handle(request, response));
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
 
