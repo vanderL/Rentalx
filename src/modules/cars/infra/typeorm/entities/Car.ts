2c53cb39765124498ac3535ed6af7690f5d9c0cc
Vander Lima <vnd.vander@gmail.com>
2022-04-26 19:21:48 -0300

    Entity Cars - Crete car in database

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,34 +1,50 @@
-// /* eslint-disable indent */
-// import {
-//   Column, CreateDateColumn, Entity, PrimaryColumn,
-// } from 'typeorm';
+/* eslint-disable indent */
+import {
+  Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+import { Category } from './Category';
+
+@Entity('cars')
 class Car {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
-  available?: boolean;
+  @Column()
+  available: boolean;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
-  created_at?: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
-      this.created_at = new Date();
       this.available = true;
     }
   }
---
+++ src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+class CreateCarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const {
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      license_plate,
+      name,
+    } = request.body;
+
+    const createCarUseCase = container.resolve(CreateCarUseCase);
+
+    const car = await createCarUseCase.execute({
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      license_plate,
+      name,
+    });
+
+    return response.status(201).json(car);
+  }
+}
+
+export { CreateCarController };
