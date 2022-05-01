d72eaea6a38f6620b5592fb5a8525fc8889eeab3
Vander Lima <vnd.vander@gmail.com>
2022-05-01 18:56:09 -0300

    Car Image: Migrations, Entity and Repository

---
+++ src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -0,0 +1,28 @@
+/* eslint-disable indent */
+import {
+  Column, CreateDateColumn, Entity, PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity()
+class CarImage {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  car_id: string;
+
+  @Column()
+  image_name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { CarImage };
