21b1acbc8e62eb197653b652a88547b3f185c336
Vander Lima <vnd.vander@gmail.com>
2022-05-07 17:46:14 -0300

    First Integration Test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,9 +1,14 @@
-import { app } from '@shared/infra/http/app';
 import request from 'supertest';
+import { app } from '@shared/infra/http/app';
 
 
 describe("Create Category Controller", () => {
-  it("Should be able test ok", async () => {
-    await request(app).get("/cars/available").expect(200)
+  it("Should be able to create a new category", async () => {
+    const response = await request(app).post("/categories").send({
+      name: "Categories Supertest",
+      description: "Categories supertest description"
+    })
+
+    expect(response.status).toBe(201)
   })
 })
