c2f3dcdd6162cf7b0cac000b02f13d88d9f3e650
Vander Lima <vnd.vander@gmail.com>
2022-11-22 00:20:24 -0300

    fix: devolution rental car

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -17,18 +17,32 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    return this.repository.findOne({ car_id });
+    return this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return this.repository.findOne({ user_id });
+    return this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
   }
 
-  async create({ car_id, expected_return_date, user_id }: ICreateRentalDTO): Promise<Rental> {
+  async create({
+    car_id,
+    expected_return_date,
+    user_id,
+    id,
+    end_date,
+    total
+  }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
       expected_return_date,
       user_id,
+      id,
+      end_date,
+      total
     });
 
     await this.repository.save(rental);
